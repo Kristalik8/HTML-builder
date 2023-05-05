0a62e3776a8934139d3c5140c06b8ae7619e68eb
Kristina <nice.kres@mail.ru>
2023-05-05 20:47:54 +0300

    feat: solve the 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const fsProm = require('fs/promises');
+const path = require('path');
+const filesD = path.join(__dirname, 'files');
+const filesCopyD = path.join(__dirname, 'files-copy');
+
+fs.mkdir(filesCopyD, { recursive: true }, (err) => {
+  if (err) console.log(err);
+});
+
+
+try {
+  (async function () {
+    const files = await fsProm.readdir(filesD);
+    const filesCopy = await fsProm.readdir(filesCopyD);
+
+    if (filesCopy.length !== 0) {
+      filesCopy.forEach((fileCopy) => {
+        fsProm.unlink(path.join(__dirname, 'files-copy', fileCopy));
+      });
+    }
+    files.forEach(file => {
+      fsProm.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+    });
+  })();
+  console.log('Копирование произошло!');
+} catch {
+  console.log('Копирование не произошло');
+}
 
