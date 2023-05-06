a832736a39698b8a6d6993a0c9d8dca2034fd236
Kristina <nice.kres@mail.ru>
2023-05-06 21:46:11 +0300

    refactor: more right code for 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-
 const textFile = path.join(__dirname, 'text.txt');
 const {stdin, stdout} = process;
 
-fs.writeFile(textFile, '', function (err) {
-  if (err) throw err;
-});
-
-
-const textFileOutput = fs.createWriteStream(textFile);
+const textFileOutput = fs.createWriteStream(textFile, {  flags: 'a'});
 
 stdin.on('data', data => {
   let text = data.toString().trim();
@@ -19,11 +13,8 @@ stdin.on('data', data => {
   textFileOutput.write(data);
 });
 
-
 process.on('SIGINT', () => {process.exit();});
 
 process.on('exit', () => stdout.write('Поздравляю, вы создали файл!'));
 
 stdout.write('\nНапишите что-нибудь\n');
-
-
